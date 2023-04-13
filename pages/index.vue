15c173e089856516bf61e76bddf2fead5b508424
unknown <franmelerogallardo@gmail.com>
2023-04-13 14:36:43 +0200

    Version funcional, sin diseño

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,29 @@
 <template>
     <div class="landing">
+        <div class="landing-box">
         <h1>SEO TOOL: Intención de búsqueda con CHAT GPT</h1>
 
        
         <div v-if="getExcelContent()"><h2>Palabras Clave:</h2><br/><p> {{ getExcelContent() }}</p>
             <button @click="resetExcelContent()">Reset</button>
+            <button @click="sendToChatGpt()">Enviar prompt a Chat GPT</button><br>
+            <p>{{ response }}</p>
         </div>
-        <div v-else><input id="excelfile" type="file" @change="uploadExcel()" /></div>
-   
+        <div v-else>
+            
+            <input id="excelfile" type="file" @change="uploadExcel()" />
+        
+            <p>Selecciona un archivo .xlsx</p>
+        </div>
+           
+    </div>
     </div>
 </template>
 
 <script setup>
 import readXlsFile from "read-excel-file";
+import { ref } from 'vue';
+const response = ref({});
 
 const data = () => ({
     items: [],
@@ -49,14 +60,56 @@ const uploadExcel = () => {
 };
 
 
+const sendToChatGpt = () => {
+  const promptContent = getExcelContent();
+  const systemMessage = [{
+    role: "system",
+    content:  `Ordena las siguientes keywords por intención de búsqueda y familias semanticas. Seria interesante que lo ordenaras para que quedaran agrupadas las keywords por diferentes paginas.  ${promptContent}`
+  }];
+  fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer 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",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: systemMessage,
+      max_tokens: 1024,
+      temperature: 0.7,
+      n: 1,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+        response.value = data.choices[0].message.content;
+    })
+    .catch((error) => console.error(error));
+};
+
+
 </script>
 <style scoped>
 .landing {
-    min-height: 89vh;
+    min-height: 94vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
 }
 
 h1 {
     text-align: center;
+}
+.landing-box{
+height: 70vh;
+border: 2px solid grey;
+border-radius: 14px;
+box-shadow: 0px 4px 6px rgba(0,0,0,0.4);
+padding: 100px;
+}
+#excelfile{
+    margin-top: 4vh;
 }
 </style>
   
