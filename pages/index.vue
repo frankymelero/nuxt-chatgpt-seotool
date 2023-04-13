01523c3b025b6357954abc5e36a7f24cffc870bf
unknown <franmelerogallardo@gmail.com>
2023-04-13 11:45:07 +0200

    page reloading fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,30 +2,53 @@
     <div class="landing">
         <h1>SEO TOOL: Intención de búsqueda con CHAT GPT</h1>
 
-        <input id="excelfile" type="file" @change="uploadExcel()" />
-
+       
+        <div v-if="getExcelContent()"><h2>Palabras Clave:</h2><br/><p> {{ getExcelContent() }}</p>
+            <button @click="resetExcelContent()">Reset</button>
+        </div>
+        <div v-else><input id="excelfile" type="file" @change="uploadExcel()" /></div>
+   
     </div>
 </template>
+
 <script setup>
 import readXlsFile from "read-excel-file";
 
 const data = () => ({
     items: [],
 });
+const getExcelContent = () => {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem('excelcontent');
+  }
+  return '';
+};
+const resetExcelContent = () => {
+  if (typeof window !== "undefined") {
+    window.localStorage.removeItem("excelcontent");
+    location.reload();
+  }
+};
 
+//Función que sube un excel y transforma las kw en una cadena de texto
 const uploadExcel = () => {
-    let excelcontent = "";
+  
     const input = document.getElementById("excelfile");
     readXlsFile(input.files[0]).then((rows) => {
         data.items = rows;
         const valores = data.items.slice(3).map(item => item[0]);
-        excelcontent = valores.join(", ");
-        console.log(excelcontent);
+        let excelcontent = valores.join(", ");
+        input.style.display="none";
+        if (typeof window !== 'undefined') {
+      window.localStorage.setItem('excelcontent', excelcontent);
+      location.reload();
+    }    
     });
 
-
-
+    
 };
+
+
 </script>
 <style scoped>
 .landing {
